c596abc9e9b27485f08e8fe541b741761fe1e4de
Andrew Kirkwood <andrewkirkwood90@gmail.com>
2020-01-07 16:20:30 +0000

    fixed describe problem

---
+++ specs/painter_decorator_spec.js
@@ -0,0 +1,25 @@
+const assert = require('assert')
+const Decorator = require('../decorator.js')
+const Room = require('../room.js')
+const Paint = require('../paint.js')
+
+describe('Room', function(){
+
+  // let room
+  // let paint
+  // let decorator
+  //
+  // beforeEach(function() {
+  //   room = new Room(3)
+  //   paint = new Paint(10)
+  //   decorator = new Decorator('Jim')
+  // })
+
+  // it('should have an area', function(){
+  //   const actual = room.area
+  //   asssert.strictEqual(actual, 3)
+  // })
+
+
+
+})
