ccc183c5884d344456fac64e56143443174ac303
Andrew Kirkwood <andrewkirkwood90@gmail.com>
2020-01-07 16:34:52 +0000

    first paint test passed

--- specs/painter_decorator_spec.js
+++ specs/painter_decorator_spec.js
@@ -3,23 +3,37 @@ const Decorator = require('../decorator.js')
 const Room = require('../room.js')
 const Paint = require('../paint.js')
 
-describe('Room', function(){
+describe('Painter Decorator', function(){
 
-  // let room
-  // let paint
-  // let decorator
-  //
-  // beforeEach(function() {
-  //   room = new Room(3)
-  //   paint = new Paint(10)
-  //   decorator = new Decorator('Jim')
-  // })
+  let room
+  let paint
+  let decorator
 
-  // it('should have an area', function(){
-  //   const actual = room.area
-  //   asssert.strictEqual(actual, 3)
-  // })
-
-
+  beforeEach(function() {
+    room = new Room(3)
+    paint = new Paint(10)
+    decorator = new Decorator('Jim')
+  })
+  describe('Room', function() {
+  it('should have an area', function(){
+    const actual = room.area
+    assert.strictEqual(actual, 3)
+  })
+  it('should start not painted', function (){
+    const actual = room.painted
+    assert.strictEqual(actual, 0)
+  })
+  it('should be able to be painted', function() {
+    room.paintRoom();
+    const actual = room.painted
+    assert.strictEqual(actual, 1)
+  })
+})
+describe('Paint', function() {
+  it('should have litres of paint', function() {
+    const actual = paint.litres
+    assert.strictEqual(actual, 10)
+  })
+})
 
 })
